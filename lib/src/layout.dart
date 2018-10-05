f4d727dd7a447cece04560660e1e5fc23892ab4f
conrad96 <conrad.mugisha@gmail.com>
2018-10-05 15:50:54 +0300

    skeleton dcode for the app

---
+++ lib/src/layout.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+class Layout extends StatefulWidget
+{
+  createState()
+  {
+    return new LayoutState();
+  }
+}
+class LayoutState extends State<Layout>
+{
+  Widget build(context)
+  {
+    
+  }
+}
