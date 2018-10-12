5a42b4dc852c1703745799220a7099af22e08d9f
conrad96 <conrad.mugisha@gmail.com>
2018-10-12 17:25:19 +0300

    navigation example between screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import "src/layout.dart";
+
 
 void main() => runApp(new CrudApp());
 
@@ -8,11 +8,43 @@ class CrudApp extends StatelessWidget
   Widget build(context)
   {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("CRUD")
-        ),
-         body: Layout()
+      title: "Navigation",
+      home: firstScreen(),
+    );
+  }
+}
+class firstScreen extends StatelessWidget
+{
+  Widget build(context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => secondScreen()));
+            },
+          child: Text("Next Screen"),
+        )
+      ),
+    );
+  }
+}
+class secondScreen extends StatelessWidget
+{
+  Widget build(context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => firstScreen()));
+        },
+            child: Text("Previous Screen")),
       ),
     );
   }
